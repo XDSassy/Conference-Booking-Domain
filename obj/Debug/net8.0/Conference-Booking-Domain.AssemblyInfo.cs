39890aa973ea41b240a6f9766a5fdbf4e4f96ce6
XDSassy <rikhardvn@gmail.com>
2026-01-26 17:57:45 +0200

    Added roomtypes.cs file

--- obj/Debug/net8.0/Conference-Booking-Domain.AssemblyInfo.cs
+++ obj/Debug/net8.0/Conference-Booking-Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Conference-Booking-Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7034f6d84c45cf2081acc5ae6918dbb17f92fb90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60ff4ea52b8d10d474c821a5020efd3f844a24c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Conference-Booking-Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Conference-Booking-Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
